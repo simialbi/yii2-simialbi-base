5db24f14491cd7f1ae23e560646ae0365873cfa8
Simon Karlen <karlen@tonic.ag>
2017-11-03 09:36:09 +0100

    added module and widget skeletons

---
+++ Module.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * @package yii2-simialbi-base
+ * @author Simon Karlen <simi.albi@gmail.com>
+ * @version 0.1
+ */
+
+namespace simialbi\yii2;
+
+/**
+ * Class Module
+ *
+ * @author Simon Karlen <simi.albi@gmail.com>
+ * @since 0.1
+ */
+class Module extends \yii\base\Module {
+
+}
---
+++ Widget.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * @package yii2-simialbi-base
+ * @author Simon Karlen <simi.albi@gmail.com>
+ * @version 0.1
+ */
+
+namespace simialbi\yii2;
+
+/**
+ * Class Widget
+ *
+ * @author Simon Karlen <simi.albi@gmail.com>
+ * @since 0.1
+ */
+class Widget extends \yii\base\Widget {
+
+}
