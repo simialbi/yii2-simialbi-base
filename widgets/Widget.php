4e1832074cb7b878da955a4c171279809ed2c10b
Simon Karlen <karlen@tonic.ag>
2017-11-03 10:19:17 +0100

    added some widget logic

--- widgets/Widget.php
+++ widgets/Widget.php
@@ -7,6 +7,9 @@
 
 namespace simialbi\yii2\widgets;
 
+use yii\helpers\Json;
+use yii\helpers\StringHelper;
+
 /**
  * Class Widget
  *
@@ -14,5 +17,73 @@ namespace simialbi\yii2\widgets;
  * @since 0.1
  */
 class Widget extends \yii\base\Widget {
+	/**
+	 * @var array the options for the underlying JS plugin.
+	 */
+	public $clientOptions = [];
+	/**
+	 * @var array the event handlers for the underlying JS plugin.
+	 */
+	public $clientEvents = [];
+	/**
+	 * @var array the HTML attributes for the widget container tag.
+	 * @see \yii\bootstrap\Html::renderTagAttributes() for details on how attributes are being rendered.
+	 */
+	public $options = [];
 
+	/**
+	 * @inheritdoc
+	 */
+	public function init() {
+		if (!isset($this->options['id'])) {
+			$this->options['id'] = $this->getId();
+		}
+		parent::init();
+	}
+
+
+	/**
+	 * Registers a specific plugin and the related events
+	 *
+	 * @param string $pluginName optional plugin name
+	 */
+	protected function registerPlugin($pluginName = null) {
+		$view = $this->view;
+
+		$className      = static::className();
+		$assetClassName = $className."Asset";
+		if (empty($pluginName)) {
+			$pluginName = strtolower(StringHelper::basename($className));
+		}
+		if (class_exists($assetClassName)) {
+			/**
+			 * @var \simialbi\yii2\web\AssetBundle $assetClassName
+			 */
+			$assetClassName::register($view);
+		}
+
+		$id = $this->options['id'];
+
+		if ($this->clientOptions !== false) {
+			$options = empty($this->clientOptions) ? '' : Json::htmlEncode($this->clientOptions);
+			$js      = "jQuery('#$id').$pluginName($options);";
+			$view->registerJs($js);
+		}
+
+		$this->registerClientEvents();
+	}
+
+	/**
+	 * Registers JS event handlers that are listed in [[clientEvents]].
+	 */
+	protected function registerClientEvents() {
+		if (!empty($this->clientEvents)) {
+			$id = $this->options['id'];
+			$js = [];
+			foreach ($this->clientEvents as $event => $handler) {
+				$js[] = "jQuery('#$id').on('$event', $handler);";
+			}
+			$this->view->registerJs(implode("\n", $js));
+		}
+	}
 }
