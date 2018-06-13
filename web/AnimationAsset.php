937f9db83232a50a92be6e0ffc550eb30c7d4c03
simialbi <simi.albi@gmail.com>
2018-06-13 15:48:01 +0200

    added animation asset

---
+++ web/AnimationAsset.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * @package yii2-simialbi-base
+ * @author Simon Karlen <simi.albi@gmail.com>
+ */
+
+namespace simialbi\yii2\web;
+
+/**
+ * Animation asset bundle vor css animations
+ * @package simialbi\yii2\web
+ * @see https://github.com/daneden/animate.css
+ */
+class AnimationAsset extends AssetBundle {
+	/**
+	 * {@inheritdoc}
+	 */
+	public $sourcePath = '@bower/animate.css';
+	/**
+	 * {@inheritdoc}
+	 */
+	public $css = [
+		'animate.min.css'
+	];
+}
